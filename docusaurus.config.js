524cee588136fbf493266f9f86a28f3c2e2aef5b
Yilbert MD <ymolina@lsv-tech.com>
2023-08-17 13:55:05 -0500

    a

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ const config = {
             position: 'left',
             label: 'Productos',
           },
-          {to: '/Docs/tutorial-basic/cattegorias', label: 'QA', position: 'left'},
+          {to: '/Docs/tutorial-basic/Cattegorias', label: 'QA', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
